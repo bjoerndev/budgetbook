aa7e1d0c426e24718679a4c2c5016547db159733
Bjoern Schaedlich <gaktion@gmx.de>
2021-03-31 08:59:33 +0200

    Improve table date rendering

--- src/main/java/de/bjrn/budgetbook/view/swing/tables/SimpleTableCellRenderer.java
+++ src/main/java/de/bjrn/budgetbook/view/swing/tables/SimpleTableCellRenderer.java
@@ -1,16 +1,18 @@
 package de.bjrn.budgetbook.view.swing.tables;
 
-import de.bjrn.budgetbook.model.Category;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
+import java.sql.Timestamp;
 
 public class SimpleTableCellRenderer extends DefaultTableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
                                                    int row, int column) {
+        if (value instanceof Timestamp) {
+            value = ((Timestamp)value).toLocalDateTime().toLocalDate();
+        }
         Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
         if (!isSelected && !hasFocus) {
             if (row % 2 == 0) {
